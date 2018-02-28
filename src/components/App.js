4e87285c4d101c00b7dc15bb085c4fc6f9284795
attachesoftware\StevenG <steveng@attachesoftware.com>
2018-02-28 19:10:21 +1100

    Style updated

--- src/components/App.js
+++ src/components/App.js
@@ -31,65 +31,140 @@ class App extends Component {
     if (keyVal && this.props.lkStore.lock.locked) {
       debugger;
       console.log('clcikeddd! - ' + keyVal);
-      this.props.lockActions.validateCurrentInput(this.props.lkStore.lock.currentInput);
-    }else if(keyVal && !this.props.lkStore.lock.locked){
-      this.props.lockActions.updateCurrentPin(this.props.lkStore.lock.currentInput);
+      this.props.lockActions.validateCurrentInput(
+        this.props.lkStore.lock.currentInput
+      );
+    } else if (keyVal && !this.props.lkStore.lock.locked) {
+      this.props.lockActions.updateCurrentPin(
+        this.props.lkStore.lock.currentInput
+      );
     }
   };
   render() {
     return (
-      <div className="App">
-        <div className="buttonPanel">
-          <div>
-            <button onClick={() => this.clearContent('clr')}>Clr</button>
+      <div className="safe-lock">
+        <div className="row">
+          <div className="buttonPanel col-md-6">
+            <div className="keypad">
+              <div className="row">
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('1')}
+                  >
+                    1
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('2')}
+                  >
+                    2
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('3')}
+                  >
+                    3
+                  </button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('4')}
+                  >
+                    4
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('5')}
+                  >
+                    5
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('6')}
+                  >
+                    6
+                  </button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('7')}
+                  >
+                    7
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('8')}
+                  >
+                    8
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('9')}
+                  >
+                    9
+                  </button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.clearContent('clr')}
+                  >
+                    CLR
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.handleClick('0')}
+                  >
+                    0
+                  </button>
+                </div>
+                <div className="col-sm-4">
+                  <button
+                    className="btn btn-primary btn-block"
+                    onClick={() => this.enterValue('Enter')}
+                  >
+                    =>
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <button onClick={() => this.handleClick('1')}>1</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('2')}>2</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('3')}>3</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('4')}>4</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('5')}>5</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('6')}>6</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('7')}>7</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('8')}>8</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('9')}>9</button>
-          </div>
-          <div>
-            <button onClick={() => this.handleClick('0')}>0</button>
-          </div>
-          <div>
-            <button onClick={() => this.enterValue('Enter')}>Enter</button>
-          </div>
-        </div>
-        <div className="displayPanel">
-          <div>
-            Display:{' '}
-            {this.props.lkStore.lock.currentInput === ''
-              ? '****'
-              : this.props.lkStore.lock.currentInput}
-          </div>
-          <div>
-            Locked Indicator:{' '}
-            {this.props.lkStore.lock.locked ? 'locked' : 'unlocked'}
-          </div>
-          <div>
-            {this.props.lkStore.lock.valid ? '' : 'INVALID'}
+          <div className="col-md-6">
+            <div className="display">
+              {!this.props.lkStore.lock.valid?"INVALID":(this.props.lkStore.lock.currentInput === ''
+                ? '****'
+                : this.props.lkStore.lock.currentInput)}
+            </div>
+            <div
+              className={
+                this.props.lkStore.lock.locked
+                  ? 'indicator locked'
+                  : 'indicator unlocked'
+              }
+            />
           </div>
         </div>
       </div>
